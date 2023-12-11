d3c9890b7608ec9eba78c3a75bd7e66b8c24891f
Anna <anna-bogacheva86@yandex.ru>
2023-12-11 22:53:27 +0500

    Тест API

---
+++ API_test_2.py
@@ -0,0 +1,17 @@
+from fastapi.testclient import TestClient
+from main import app
+from main import Item
+
+client = TestClient(app)
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello, World!"}
+
+def test_predict():
+    item = Item(context="The Universe contains everything that exists", question="What is at the center of the solar system?")
+    response = client.post("/predict/", json=item.dict())
+    assert response.status_code == 200
+    assert "answer" in response.json()
+    assert "score" in response.json()
